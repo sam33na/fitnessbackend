5a0e62755d0cd353d5f5ee62a22618c24046459c
sam33na <saminashrestha98@gmail.com>
2021-06-22 22:38:11 +0545

    login success

--- route/loginRoute.js
+++ route/loginRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Login = require('../model/login_model');
+const Register = require('../model/login_model');
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 // const auth = require('../middleware/auth');
 // const upload = require('../middleware/upload');
 
-
+//user register
 router.post('/register/user',  function (req, res) {
     const errors = validationResult(req);
     //(!errors.isEmpty())
@@ -20,7 +20,7 @@ router.post('/register/user',  function (req, res) {
         const phone = req.body.phone;
         const role = req.body.role;
         bcryptjs.hash(password, 10, function (err, hide) {
-            const store = new Login({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
+            const store = new Register({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
             console.log(hide);
             store.save().then(function (result) {
                 res.status(200).json({ success: true, message: "Registeration sucessfull" }) 
@@ -36,4 +36,28 @@ router.post('/register/user',  function (req, res) {
     }
 })
 
+//login 
+router.post('/user/login', function (req, res) {
+    const user = req.body.username;
+    const password = req.body.password;
+    Register.findOne({ username: user }).then(function (pulledData) {
+        if (pulledData === null) {
+            return res.status(201).json({ success: false, message: "Invalid Details" }) //username false huda
+        }
+        bcryptjs.compare(password, pulledData.password, function (err, result) {
+            if (result === false) {
+                return res.status(201).json({ msg: "Invalid credentials" })//pw incorct
+            }
+            const token = jwt.sign({ userId: pulledData._id }, 'anysecretkey');
+            return res.status(200).json({ success: true, 
+                msg: "successfull authentication", 
+                token: token, 
+                role: pulledData.role,
+                data: pulledData._id })
+        })
+    })
+        .catch(function (e) {
+            res.status(500).json({ message: e })
+        })
+})
 module.exports = router;
