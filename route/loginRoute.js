0ae0697f9c6ff1f015a829a4ce3620ba89944020
sam33na <saminashrestha98@gmail.com>
2021-06-22 22:28:04 +0545

    user registered

--- route/loginRoute.js
+++ route/loginRoute.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const Login = require('../model/login_model');
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const auth = require('../middleware/auth');
-const upload = require('../middleware/upload');
+// const jwt = require('jsonwebtoken')
+// const auth = require('../middleware/auth');
+// const upload = require('../middleware/upload');
 
 
-router.post('/user/register',  function (req, res) {
+router.post('/register/user',  function (req, res) {
     const errors = validationResult(req);
     //(!errors.isEmpty())
     if (errors.isEmpty()) {
@@ -20,7 +20,7 @@ router.post('/user/register',  function (req, res) {
         const phone = req.body.phone;
         const role = req.body.role;
         bcryptjs.hash(password, 10, function (err, hide) {
-            const store = new Register({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
+            const store = new Login({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
             console.log(hide);
             store.save().then(function (result) {
                 res.status(200).json({ success: true, message: "Registeration sucessfull" }) 
@@ -36,4 +36,4 @@ router.post('/user/register',  function (req, res) {
     }
 })
 
-
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const mongoose=require('mongoose');
 const express=require('express');
-const path = require('path')
 const bodyParser =require('body-parser');
-const cors = require('cors')
+// const path = require('path')
+// const cors = require('cors')
 
 const db= require('./database/db');
+const login_route=require('./route/loginRoute');
 
 const app = express();//core module
 app.use(express.json());
-
+app.use(login_route);
 
 app.listen(90);
